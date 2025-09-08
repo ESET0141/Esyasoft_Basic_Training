d372ac44637af94dde5dba7f121fce7a8a932359
ESET0141 <tejas.sandwar@esyasoft.com>
2025-09-08 20:51:58 +0530

    removed button

--- Basics of Embedded Systems/assmt_3/src/three_led.c
+++ Basics of Embedded Systems/assmt_3/src/three_led.c
@@ -5,13 +5,13 @@
 #define LED_1 PD2
 #define LED_2 PD5
 #define LED_3 PD7
-#define BUTTON PD6
+
 
 int main(){
  DDRD |= (1 << LED_1);
  DDRD |= (1 << LED_2);
  DDRD |= (1 << LED_3);
-//  DDRD &= (1 << BUTTON);
+
 
 PORTD |= (1 << LED_1);
 PORTD &= ~(1 << LED_2);
