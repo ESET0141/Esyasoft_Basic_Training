4dd331181458b31dc829701bcf4e63fda4b238ab
ESET0141 <tejas.sandwar@esyasoft.com>
2025-09-18 13:02:31 +0530

    Demonstration of Data Strucutres in C#

---
+++ Basics of dotNet/DS_Demo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace Data_Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List Example
+            List<Student> students = new List<Student>();
+
+            // Add Student objects to the list
+            Student first = new Student(1, "Alice", 10);
+            Student second = new Student(2, "Bob", 90);
+            Student third = new Student(3, "Charlie", 78);
+            students.Add(first);
+            students.Add(second);
+            students.Add(third);
+
+
+            // Access and display each student using foreach
+            Console.WriteLine("Student List:");
+            foreach (Student s in students)
+            {
+                Console.WriteLine($"ID: {s.id}, Name: {s.name}, Marks: {s.marks}");
+            }
+
+            // Access a specific object by index
+            Console.WriteLine($"\nSecond student is: {students[1].name}");
+
+
+            //Dictionay Example
+            Dictionary<string, Student> students_dict = new Dictionary<string, Student>();
+            students_dict.Add("firstStudent", first);
+            students_dict.Add("seondStudent", second);
+            students_dict.Add("thirdStudent", third);
+
+
+            foreach (KeyValuePair<string, Student> student in students_dict)
+            {
+                Console.WriteLine(student.Value.id);
+            }
+
+
+            //Hashset Example
+            Console.WriteLine("Hashset demo");
+            HashSet<Student> students_hashset = new HashSet<Student>();
+            students_hashset.Add(first);
+            students_hashset.Add(second);
+            students_hashset.Add(first);
+            students_hashset.Add(third);
+
+            foreach (Student student in students_hashset)
+            {
+                Console.WriteLine(student.id);
+            }
+
+
+            //StackDemo
+
+
+            Stack<Student> students_stack = new Stack<Student>();
+            students_stack.Push(first);
+            students_stack.Push(second);
+            students_stack.Push(third);
+
+            Student pop_stack = students_stack.Pop();
+            Console.WriteLine(pop_stack.name);
+
+
+            //Queue demo
+            Console.WriteLine("Queue demo");
+            Queue<Student> students_queue = new Queue<Student>();
+            students_queue.Enqueue(first);
+            students_queue.Enqueue(second);
+            students_queue.Enqueue(third);
+
+            Console.WriteLine(students_queue.Dequeue().name);
+
+            // demo
+            Console.WriteLine("LinkedList :");
+            LinkedList<Student> students_LinkedList = new LinkedList<Student>();
+
+            students_LinkedList.AddLast(first);
+            students_LinkedList.AddLast(second);
+            students_LinkedList.AddLast(third);
+
+            Console.WriteLine("Students are :");
+
+            foreach (var s in students_LinkedList)
+            {
+                Console.WriteLine($"ID: {s.id}, Name: {s.name}, Marks: {s.marks}");
+            }
+
+            //
+            Console.WriteLine("Tuple :");
+
+            var student_t = Tuple.Create(first, second, third);
+            Console.WriteLine(student_t.Item1.name);
+            Console.WriteLine(student_t.Item2.name);
+            Console.WriteLine(student_t.Item3.name);
+        }
+    }
+}
